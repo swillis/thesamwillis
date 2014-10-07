b71fa4172565dc34b082b872ae10cfe004701e68
Sam Willis <samueljohnwillis@gmail.com>
2014-10-07 18:25:15 +0100

    implement fullpage.js

--- source/javascripts/_main.js
+++ source/javascripts/_main.js
@@ -1,5 +1,10 @@
-$('.content-slider').bxSlider({
-  infiniteLoop: false,
-  hideControlOnEnd: true,
-  mode: 'vertical'
+$(document).ready(function() {
+	$('.slider').fullpage({
+		anchors: ['firstPage', 'secondPage', '3rdPage'],
+		navigation: true,
+		navigationPosition: 'right',
+		navigationTooltips: ['First page', 'Second page', 'Third and last page']
+	});
 });
+
+// console.log("Message from Sam: 'Checking under the hood huh? Can't blame you, but maybe take a look at some of the cool work I've done instead?'");
--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,4 +1,13 @@
+// Vendor
+
 //= require "vendor/_jquery.min"
-//= require "vendor/_jquery.bxslider.min"
+
+//= require "vendor/_jquery.easings.min"
+
+//= require "vendor/_jquery.slimscroll.min"
+
+//= require "vendor/_jquery.fullPage.min"
+
+// Main
 
 //= require "_main.js"
