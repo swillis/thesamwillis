5acc0538f7086b0b403f45b10534a5ca865f356a
Sam Willis <samueljohnwillis@gmail.com>
2014-10-29 18:11:35 +0000

    start placeholder

--- source/javascripts/_main.js
+++ source/javascripts/_main.js
@@ -1,10 +1,1 @@
-$(document).ready(function() {
-	$('.slider').fullpage({
-		anchors: ['firstPage', 'secondPage', '3rdPage'],
-		navigation: true,
-		navigationPosition: 'right',
-		navigationTooltips: ['First page', 'Second page', 'Third and last page']
-	});
-});
-
 // console.log("Message from Sam: 'Checking under the hood huh? Can't blame you, but maybe take a look at some of the cool work I've done instead?'");
