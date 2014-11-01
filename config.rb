d804225723e84c9f2895f120d76ba9efabc88fbb
Sam Willis <samueljohnwillis@gmail.com>
2014-11-01 01:03:38 +0000

    add animations

--- config.rb
+++ config.rb
@@ -45,11 +45,11 @@
 #   end
 # end
 
-set :css_dir, 'stylesheets'
+set :css_dir, 'thesamwillis/stylesheets'
 
-set :js_dir, 'javascripts'
+set :js_dir, 'thesamwillis/javascripts'
 
-set :images_dir, 'images'
+set :images_dir, 'thesamwillis/images'
 
 # Build-specific configuration
 configure :build do
--- source/javascripts/_main.js
+++ source/javascripts/_main.js
@@ -1,1 +1,9 @@
 // console.log("Message from the author: 'Hey there'");
+
+$('.details').addClass('animated fadeInRight');
+
+$('.profile-picture').addClass('animated fadeInLeft');
+
+$('.title').addClass('animated fadeInDown');
+
+$('.social-icons').addClass('animated fadeInUp');
