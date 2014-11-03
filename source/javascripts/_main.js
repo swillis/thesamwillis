27393174f4319e87aa64b499a07edd292261f5ed
Sam Willis <samueljohnwillis@gmail.com>
2014-11-03 00:38:25 +0000

    tweak loading animation, add console message

--- source/javascripts/_main.js
+++ source/javascripts/_main.js
@@ -1,11 +1,13 @@
-// console.log("Message from the author: 'Hey there'");
+console.log("Message from the author: Thanks for visiting! If you have any tips on how I can improve this site, please email them to hello@thesamwillis.com.");
 
-$('.details').addClass('animated fadeInRight');
+$(window).load(function() {
+	$('.details').removeClass('hide').addClass('animated fadeInRight');
 
-$('.profile-picture').addClass('animated fadeInLeft');
+	$('.profile-picture').removeClass('hide').addClass('animated fadeInLeft');
 
-$('.title').addClass('animated fadeInDown');
+	$('.title').removeClass('hide').addClass('animated fadeInDown');
 
-$('.social-icons').addClass('animated fadeInUp');
+	$('.social-icons').removeClass('hide').addClass('animated fadeInUp');
+});
 
-$('.email').popover()
+$('.email').popover();
